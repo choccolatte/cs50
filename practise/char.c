5b0d52b9cb3cb13ec9e10eae9aef5f1c24e07e2b
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-04-24 19:57:32 +0530

    used a for loop instead

--- practise/char.c
+++ practise/char.c
@@ -5,9 +5,14 @@ int main (void)
     char *s = "Hi!";
 
     printf("%s\n", s); // will print the entire string
-    printf("%c\n", s[0]); // will print string characrter-wise
-    printf("%c\n", s[1]); // will print string characrter-wise
-    printf("%c\n", s[2]); // will print string characrter-wise
-    printf("%c\n", s[3]); // will print string characrter-wise
+    // printf("%c\n", s[0]); // will print string characrter-wise
+    // printf("%c\n", s[1]); // will print string characrter-wise
+    // printf("%c\n", s[2]); // will print string characrter-wise
+    // printf("%c\n", s[3]); // will print string characrter-wise
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%c\n", s[i]);
+    }
 
 }
