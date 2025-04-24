97daa6a8bc1044769f709657bf8aef843248bf0c
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-04-24 19:46:32 +0530

    playing with char* - 2404251951

---
+++ practise/char.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (void)
+{
+    char *s = "Hi!";
+
+    printf("%s\n", s); // will print the entire string
+    printf("%c\n", s[0]); // will print string characrter-wise
+    printf("%c\n", s[1]); // will print string characrter-wise
+    printf("%c\n", s[2]); // will print string characrter-wise
+    printf("%c\n", s[3]); // will print string characrter-wise
+
+}
